02814f2cac081d95bf908bf374f6b9287736bed4
Andrey <andrei.svistun@awtg.co.uk>
2021-05-23 19:21:27 +0300

    fixed function

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -11,27 +11,25 @@ router.post('/signup', (req, res) => {
         passwordhash: bcrypt.hashSync(req.body.user.password, 10),
         email: req.body.user.email,
     })
-        .then(
-            function signupSuccess(user) {
-                let token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
-                res.status(200).json({
-                    user: user,
-                    token: token
-                })
-            },
+        .then((user) => {
+            const token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
 
-            function signupFail(err) {
-                res.status(500).send(err.message)
-            }
-        )
+            res.status(200).json({
+                user: user,
+                token: token
+            })
+        }).catch((err) => {
+            res.status(500).send(err.message)
+        })
 })
 
 router.post('/signin', (req, res) => {
-    User.findOne({ where: { username: req.body.user.username } }).then(user => {
-        if (user) {
+    User.findOne({ where: { username: req.body.user.username } })
+        .then(user => {
             bcrypt.compare(req.body.user.password, user.passwordHash, function (err, matches) {
                 if (matches) {
-                    var token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+                    const token = jwt.sign({ id: user.id }, 'lets_play_sum_games_man', { expiresIn: 60 * 60 * 24 });
+
                     res.json({
                         user: user,
                         message: "Successfully authenticated.",
@@ -41,11 +39,10 @@ router.post('/signin', (req, res) => {
                     res.status(502).send({ error: "Passwords do not match." })
                 }
             });
-        } else {
-            res.status(403).send({ error: "User not found." })
-        }
 
-    })
+        }).catch(() => {
+            res.status(403).send({ error: "User not found." })
+        })
 })
 
 module.exports = router;
--- db.js
+++ db.js
@@ -5,19 +5,17 @@ const sequelize = new Sequelize(
     process.env.DB_USER,
     process.env.DB_PASS,
     {
-        dialect: process.env.DB_DIALECT,
         host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
     }
 )
 
-sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+try {
+    sequelize.authenticate();
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+    console.log("Connected to DB");
+} catch {
+    console.log(`Error: ${err}`);
+}
 
 module.exports = sequelize
